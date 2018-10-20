94868791f8511a1897b8cd5769ce1953023d6f4b
darkestmidnight <elly.richardson18@gmail.com>
2018-10-20 14:19:03 -0500

    added employees present page

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path, include
 from . import views
 
+app_name='logged_count'
 
 # pages for the webapp.
 urlpatterns = [
-    re_path(r'^$', views.logged, name="logged_users"),
+    re_path(r'^$', views.logged_count, name="logged_count"),
+    re_path(r'^loggedusers/$', views.logged, name="logged_users"),
 ]
--- index/views.py
+++ index/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from .models import LoggedUser
+from django.views.decorators.csrf import requires_csrf_token
 
 # Create your views here.
 
 def logged(request):
+  #collects the logged users  
   logged_users = LoggedUser.objects.all().order_by('username')
   context = {
       'logged_users':logged_users,
@@ -16,3 +18,13 @@ def logged(request):
   #return render_to_response('index/index.html',
   #                          {'logged_users': logged_users},
   #                          context_instance=RequestContext(request))
+
+@requires_csrf_token
+def logged_count(request):
+    logged_user_count = LoggedUser.objects.count()
+
+    context = {
+        'logged_user_count': logged_user_count,
+    }
+
+    return render(request, 'index/home.html', context)
