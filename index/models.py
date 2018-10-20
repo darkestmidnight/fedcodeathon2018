71d4027b923c719c82adafc5f943529a4a4a6fb1
darkestmidnight <elly.richardson18@gmail.com>
2018-10-20 11:01:42 -0500

    added index app

--- index/models.py
+++ index/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Recognition(models.Model):
-    name = models.Charfield(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
+#class Recognition(models.Model):
+#    name = models.Charfield(max_length=100)
+#    email = models.EmailField()
+#    date = models.DateTimeField(auto_now_add=True)
